484ae84456e4d014f863bc3f9dbf3f4bc34a7a55
hariom <Hariomvirkhare02@gmail.com>
2026-05-15 21:29:36 +0530

    Some Changes

--- apps/backend/cmd/server/main.go
+++ apps/backend/cmd/server/main.go
@@ -89,7 +89,7 @@ func main() {
 
 	// Configure CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:3002"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
